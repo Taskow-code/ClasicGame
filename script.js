c2ef3d65cfdcca0c0b44827533f7e41f0b77a4df
Taskow-code <aymanturquet16@gmail.com>
2024-07-26 15:40:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function play(userchoice) {    
+    const choices = ['pierre','papier','ciseaux'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    
+    let result = "";
+    if (userchoice === computerChoice){
+        result = 'égalité!';
+    } else if (
+        (userchoice === 'pierre' && computerChoice === 'ciseaux') ||
+        (userchoice === 'papier' && computerChoice === 'pierre') ||
+        (userchoice === 'ciseaux' && computerChoice === 'papier')
+    ){
+        result = 'Vous gagnez!';
+        playerScore++;
+    } else {
+        result = 'Vous perdez';
+        computerScore++;
+    }
+
+
+document.getElementById('result').innerText = `
+        Vous avez choisi ${userchoice}.
+        l'ordinateur a choisi ${computerChoice}. 
+        ${result}
+        `;
+        
+document.getElementById('score').textContent = `Score: Joueur ${playerScore} - Ordinateur ${computerScore}`;
+}
